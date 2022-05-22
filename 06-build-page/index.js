f9daa9bd2b075899f1081b1a3366b13f4edfd974
danil <dmahov228@gmail.com>
2022-05-22 17:47:16 +0300

    feat: make 6 part of task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const path = require('path');
+
+
 fs.mkdir(path.join(__dirname, 'project-dist'),{recursive:true}, () => {});
 fs.readdir(path.join(__dirname, 'styles'), (err, data) => {
   if(err) throw err;
@@ -26,48 +28,68 @@ fs.readdir(path.join(__dirname, 'styles'), (err, data) => {
   });
 });
 
+/* styles copy top */
 
 
-fs.readFile(path.join(__dirname, 'template.html'), function(err, content) {
-  if(err) {
-    throw err;
-  }else {
-    const data = Buffer.from(content);
-    const dataToStr = data.toString();
-    console.log(dataToStr);
-    fs.readdir(path.join(__dirname, 'components'), (err, datahtml) => {
-      if(err) throw err;
-      datahtml.forEach(elem => {
-        let htmlStr = elem.split('.')[0];
-        if(dataToStr.indexOf(htmlStr)) {
-          console.log('true');
+
+
+/* assets copy */
+function copyDir(filesPath,copiedPath) {
+  fs.readdir(filesPath, { withFileTypes: true }, (err,data)=> {
+    if (err) {
+      throw err;
+    }
+    else {
+      data.forEach(elem=> {
+        if (elem.isFile()) {
+          fs.copyFile(path.join(filesPath, elem.name), path.join(copiedPath, elem.name), err => {
+            if (err) throw err;
+          }); 
+        }
+        else if (elem.isDirectory()) {
+          fs.mkdir(copiedPath+'\\'+elem.name,{recursive:true}, err => {
+            if (err) throw err;
+          });
+          copyDir(path.join(filesPath, elem.name),path.join(copiedPath, elem.name));
         }
       });
-    });
-  }
-});
+    } 
+  });
+}
+copyDir(path.join(__dirname, 'assets'), path.join(__dirname, 'project-dist') + '\\' + 'assets');
 
-// function copyFolder(from, to) {
-//   fs.readdir(path.join(__dirname, from), (err, data) => {
-//     console.log(data);
+
+/* html copy htmplate  to project (unblock when str replace will be done) */  
+// function copyHTML(to) {
+//   fs.mkdir(path.join(__dirname, to, 'assets'),{recursive:true}, () => {});
+//   fs.readdir(path.join(__dirname), (err, data) => {
 //     if (err) {throw err;}
 //     data.forEach(elem => {
-//       fs.readdir(path.join(__dirname, from, elem), (err, datass) => {
-//         if (err) {throw err;}
-//         console.log(datass);
-//         datass.forEach(elements => {
-//           fs.copyFile(path.join(__dirname, from, elem, elements), path.join(__dirname, to, elem), err => {if (err) throw err;});
-//         });
-//       });
+//       if(elem === 'template.html' ) {
+//         fs.copyFile(path.join(__dirname, elem), path.join(__dirname, to, elem), err => {if (err) throw err;});
+//       }
 //     });
 //   });
 // }
-
-// copyFolder('assets', 'project-dist');
-
-
-/* html import */
-fs.readdir(path.join(__dirname, 'components'), (err, datahtml) => {
-  if(err) throw err;
-  datahtml.forEach(elem => console.log(elem.split('.')[0]));
+// copyHTML('project-dist');
+/* */
+let templateTxt = '';
+fs.readFile(path.join(__dirname, 'template.html'), 'utf-8', (err, templateData) => {
+  if (err) throw err;
+  templateTxt += templateData;
+  fs.readdir(path.join(__dirname, 'components'), {withFileTypes: true}, (err, files) => {
+    if(err) throw err;
+    files.forEach(file => {
+      if(file.isFile() && path.extname(file.name) === '.html') {
+        fs.readFile(path.join(path.join(__dirname, 'components'), file.name), 'utf-8', (err, data) => {
+          if(err) throw err;
+          const compName ='{{' + path.parse(file.name).name + '}}';
+          templateTxt = templateTxt.replace(compName, data);
+          fs.writeFile(path.join(__dirname, 'project-dist', 'template.html'), templateTxt, err => {
+            if(err) throw err;
+          });
+        });
+      }
+    });
+  });
 });
